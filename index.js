0bbaf8f6d986a34e7f0e5c5a0a426e32136802c8
rana135 <rana286090@gmail.com>
2022-06-08 12:52:31 +0600

    comment modify

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const res = require('express/lib/response');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -12,20 +11,70 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ejqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("dentist").collection("services");
+        const bookingCollection = client.db("dentist").collection("booking");
+
+        // get all booking or data load:-
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const cursor = bookingCollection.find(query);
+            const bookings = await cursor.toArray();
+            res.send(bookings)
+        })
+        // get all service or data load:-
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
         })
-    } finally {
+        // Warning: This is not the proper way to query multiple collection. 
+        // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            // step 1:  get all services:-
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service Bookings: ['', '', '', '']
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                //step 7: set available to slots to make it easier 
+                service.slots = available;
+            });
+
+
+            res.send(services);
+        })
+
+        // Create new Collection and added by specific booking:-
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            res.send({ success: true, result })
+        })
+    }
+    finally {
         // await client.close();
     }
 }
@@ -39,3 +88,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Dentist Server listening on port ${port}`)
 })
+
+/**
+    * API Naming Convention
+    * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+    * app.get('/booking/:id') // get a specific booking 
+    * app.post('/booking') // add a new booking
+    * app.patch('/booking/:id) //   updated only one booking
+    * app.delete('/booking/:id) //  specific service item delete
+   */
